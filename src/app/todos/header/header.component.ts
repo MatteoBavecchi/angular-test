abda19a49e9c33554763ab3c383bdf40a4261cd9
matteobavecchi <matteo.bavecchi@stud.unifi.it>
2021-04-19 11:25:10 +0200

    Header component finished with addTodo function

--- src/app/todos/header/header.component.ts
+++ src/app/todos/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { TodosService } from "../services/todos.service";
+import { TodoInterface } from "../types/todo.interface";
 
 @Component({
     selector: "app-todos-header",
@@ -6,12 +8,15 @@ import { Component } from "@angular/core";
 })
 export class HeaderComponent {
     text: string = '';
+
+    constructor(private todoService: TodosService) { }
     changeText(event: Event): void {
         const target = event.target as HTMLInputElement;
         this.text = target.value;
         console.log(target.value);
     }
     addTodo(): void {
-        console.log("addTodo", this.text);
+        this.todoService.addTodo(this.text);
+        this.text="";
     }
 }
--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { TodosComponent } from "src/app/todos/components/todos/todos.component";
 import { RouterModule, Routes } from "@angular/router";
 import { HeaderComponent } from "src/app/todos/header/header.component";
+import { TodosService } from "./services/todos.service";
 
 const routes: Routes = [
     {
@@ -12,5 +13,6 @@ const routes: Routes = [
 @NgModule({
     declarations: [TodosComponent, HeaderComponent],
     imports: [RouterModule.forChild(routes)],
+    providers: [TodosService]
 })
 export class TodosModule { }
